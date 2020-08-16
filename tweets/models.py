8fd6a35b703f76784e756e06bee478ff0737d97a
ks-desmon <kundansinghynr@gmail.com>
2020-08-16 13:57:38 +0530

    first model created

---
+++ tweets/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+    content = models.TextField(null=True , blank=True)
+    image = models.FileField(upload_to='images/',blank=True,null=True)
