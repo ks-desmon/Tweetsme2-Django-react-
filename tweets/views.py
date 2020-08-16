41edd9eed854ae7042a5dcb28da9b1336b82ded8
ks-desmon <kundansinghynr@gmail.com>
2020-08-16 14:21:54 +0530

    Dynamic Url/routing created

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    print(args , kwargs)
+    return HttpResponse('Hello world')
+
+def tweet_detail_view(request,tweet_id, *args, **kwargs):
+    print(args , kwargs)
+    return HttpResponse(f'{tweet_id}Hello world')
