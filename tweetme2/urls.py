23e63765f481fe7fe1c5ab5938e0618daf81d922
ks-desmon <kundansinghynr@gmail.com>
2020-08-16 20:22:24 +0530

    hard code form submit

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -20,7 +20,7 @@ from tweets.views import home_view , tweet_detail_view ,tweet_list_view, tweet_c
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
-    path('tweet-create', tweet_create_view),
+    path('create-tweet/', tweet_create_view),
     path('tweets/', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
 ]
