a7bc7a0f740540555121771b02a8a9a6d7c78d15
ks-desmon <kundansinghynr@gmail.com>
2020-08-16 14:34:46 +0530

    handling routing try/except

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse , Http404
+from .models import Tweet
 # Create your views here.
 
 def home_view(request, *args, **kwargs):
@@ -7,5 +8,9 @@ def home_view(request, *args, **kwargs):
     return HttpResponse('Hello world')
 
 def tweet_detail_view(request,tweet_id, *args, **kwargs):
-    print(args , kwargs)
-    return HttpResponse(f'{tweet_id}Hello world')
+    # print(args , kwargs)
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+    except:
+        raise Http404
+    return HttpResponse(f'{tweet_id}Hello world {obj.content}')
