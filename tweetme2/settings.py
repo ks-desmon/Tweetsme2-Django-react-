9bd7c12c85992c6e94598a088ab9b5295ac928e7
ks-desmon <kundansinghynr@gmail.com>
2020-08-18 19:47:46 +0530

    installed django REST framework

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # user define
     'tweets',
+    # third-party
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
